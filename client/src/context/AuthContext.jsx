3dccfa7d9b993c56df814f98e3ed674955f8e02e
natkhatishigh <iamshivanshsood@gmail.com>
2025-08-22 15:41:31 +0530

    Started using Context API to register user

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,29 @@
+import { createContext, useCallback } from "react";
+import { useState } from "react";
+
+export const AuthContext = createContext()
+
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [registerInfo, setRegisterInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  console.log("registerInfo", registerInfo);
+
+  const updateRegisterInfo = useCallback((info) => {
+    setRegisterInfo(info);
+  }, [])
+
+  return <AuthContext.Provider
+    value={{
+      user,
+      registerInfo,
+      updateRegisterInfo,
+    }}
+  >
+    {children}
+  </AuthContext.Provider>
+}
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,12 @@
+import { useContext } from "react"
 import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
+import { AuthContext } from "../context/AuthContext";
+
 
 const Register = () => {
+
+  const { registerInfo, updateRegisterInfo } = useContext(AuthContext);
+
   return (
     <>
       <Form>
@@ -16,9 +22,14 @@ const Register = () => {
             <Stack gap={3}>
               <h2>Register</h2>
 
-              <Form.Control type="text" placeholder="Name" />
-              <Form.Control type="email" placeholder="Email" />
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control type="text" placeholder="Name" onChange={(e) => updateRegisterInfo
+                ({ ...registerInfo, name: e.target.value })} />
+
+              <Form.Control type="email" placeholder="Email" onChange={(e) => updateRegisterInfo
+                ({ ...registerInfo, email: e.target.value })} />
+
+              <Form.Control type="password" placeholder="Password" onChange={(e) => updateRegisterInfo
+                ({ ...registerInfo, password: e.target.value })} />
 
               <Button variant="primary" type="submit">
                 Register

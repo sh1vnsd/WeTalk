e3bd92b0e9df08550aed59558ced98a38e3440b3
natkhatishigh <iamshivanshsood@gmail.com>
2025-08-23 18:04:17 +0530

    feat: implemented the feature to get online users and start chat w/ them

---
+++ client/src/components/chat/UserChat.jsx
@@ -0,0 +1,29 @@
+import { useFetchRecipientUser } from "../../hooks/useFetchRecipient";
+import { Stack } from "react-bootstrap";
+import avatar from "../../assets/avatar.svg"
+
+export const UserChat = ({ chat, user }) => {
+  const { recipientUser } = useFetchRecipientUser(chat, user)
+
+  return <Stack direction="horizontal" gap={3} className="user-card align-items-center p-2 justify-content-between" role="button">
+    <div className="d-flex">
+      <div className="me-2">
+        <img src={avatar} height="35px" />
+      </div>
+
+      <div className="text-content">
+        <div className="name">{recipientUser?.name}</div>
+        <div className="text">Text Message</div>
+      </div>
+    </div>
+    <div className="d-flex flex-column align-items-end">
+      <div className="date">
+        2025-08-23
+      </div>
+      <div className="this-user-notifications">2</div>
+
+      <span className="user-online"></span>
+    </div>
+  </Stack>
+}
+
